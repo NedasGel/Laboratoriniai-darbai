220b8bc404f93654543a87d877435cf0a0e15013
NedasGel <nedas.gelumbeckas@stud.vilniustech.lt>
2026-09-14 15:08:50 +0300

    Padariau galutinius punktus, prisiminiau ivarias funkcijas ir ju panaudojima help bei doc pagalba, ir parasiau pora pavyzdziu

--- LD1/LD1_NedasGelumbeckas.m
+++ LD1/LD1_NedasGelumbeckas.m
@@ -3,10 +3,51 @@
 % Grupė: EDIF-25/2
 % Data: 2026-09-14
 
+%% Pagrindinis skriptas
+
 x = 1:32;
 y = x.^2;
 
-plot(x,y, 'o-r', x, y/3, 'xb')
+plot(x,y,'o-r',x,y/3,'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-0-]   |   F_2 [-x-]')
+
+
+%% Pagalba
+
+% help sin
+% help plot
+% help title
+
+% doc sin
+% doc plot
+% doc title
+
+
+%% linspace, size ir max
+
+% help linspace
+% help size
+% help max
+
+% doc linspace
+% doc size
+% doc max
+
+clear all
+
+x2 = linspace(0,2*pi,10);
+y2 = sin(x2);
+
+dydis = size(x2);
+didziausia = max(y2);
+
+
+%% Matricos pavyzdys
+clear all
+A = [1 2 3; 4 5 6; 7 8 9];
+
+[m,n] = size(A);
+max_stulpeliai = max(A);
+max_reiksme = max(A(:));
